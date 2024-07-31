b761c57a5bd5486187c2d83049a1e856215b08f4
Lamont Crook <lamont@adobe.com>
2024-07-31 15:03:41 -0600

    update models

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,17 +1,38 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
+import { loadFragment } from '../fragment/fragment.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
+  [...block.children].forEach(async (row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
+
+    const anchor = li.querySelector('a');
+    if (anchor) {
+      console.log(anchor);
+      const { href } = anchor
+      // const resp = await fetch(`${href}.plain.html`);
+
+      console.log(href);
+
+      const frag = await loadFragment(new URL(href).pathname);
+      console.log(frag);
+
+      const pic = frag.querySelector('picture');
+
+      const div = document.createElement('div');
+      div.className = 'cards-card-image';
+      div.appendChild(pic);
+      li.replaceChildren(div);
+    }
+
+    // [...li.children].forEach((div) => {
+    //   if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+    //   else div.className = 'cards-card-body';
+    // });
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => {
